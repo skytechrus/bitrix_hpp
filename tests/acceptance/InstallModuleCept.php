18cf430f88d60955fc7c77310e7231cbe8e35045
msvbell <sv@skytecrussia.com>
2017-09-25 16:54:36 +0300

    добавил заполнение поля redirect_url в настройках

--- tests/acceptance/InstallModuleCept.php
+++ tests/acceptance/InstallModuleCept.php
@@ -7,6 +7,9 @@ $I->amOnPage('bitrix/admin/');
 $I->fillField('USER_LOGIN', 'admin');
 $I->fillField('USER_PASSWORD', 'q3sermon');
 $I->click('Login');
+$I->wait(10);
+//$I->click('//*[@id="bx-admin-prefix"]/div[2]/div[1]/div[2]/div/div[2]/input');
+//$I->click('Закрыть');
 $I->waitForText('Администрирование');
 
 /*
@@ -17,7 +20,7 @@ $I->waitForText('Администрирование');
 $I->waitForText('Магазин');
 $I->click('#global_menu_store');
 $I->waitForText('Настройки');
-$I->click('//*[@id="_global_menu_store"]/div[8]/div[1]/a');
+//$I->click('//*[@id="_global_menu_store"]/div[8]/div[1]/a'); // развернуть меню настройки
 $I->waitForText('Платежные системы');
 $I->wait(5);
 $I->click('Платежные системы');
@@ -29,6 +32,7 @@ $I->uncheckOption('PAYSYSTEMBizVal[MAP][PAYSYSTEM_NEW][MERCHANT_ID][0][DELETE]')
 $I->uncheckOption('PAYSYSTEMBizVal[MAP][PAYSYSTEM_NEW][SECRET_KEY][0][DELETE]');
 $I->uncheckOption('PAYSYSTEMBizVal[MAP][PAYSYSTEM_NEW][IS_TEST][0][DELETE]');
 $I->fillField('PAYSYSTEMBizVal[MAP][PAYSYSTEM_NEW][IS_TEST][0][PROVIDER_VALUE]', 'Y');
+$I->fillField('PAYSYSTEMBizVal[MAP][PAYSYSTEM_NEW][REDIRECT_URL][0][PROVIDER_VALUE]', 'http://localhost:8181/bitrix/tools/skytech_callback.php');
 $I->fillField('PAYSYSTEMBizVal[MAP][PAYSYSTEM_NEW][MERCHANT_ID][0][PROVIDER_VALUE]', '5352df99-9aad-48ec-bf43-f14c1b4912c3');
 $I->fillField('PAYSYSTEMBizVal[MAP][PAYSYSTEM_NEW][SECRET_KEY][0][PROVIDER_VALUE]', 'bdbbd449-33c3-4750-afdd-f5d6544e5276');
 $I->click('save'); // добавить платежную систему
